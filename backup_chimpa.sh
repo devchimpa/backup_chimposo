268af407c2a8f0085d1ea9e92021ebef6e51906e
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2023-01-09 11:22:52 -0300

    Update backup_chimpa.sh
    Enviando backup para pasta de backups na Home

--- backup_chimpa.sh
+++ backup_chimpa.sh
@@ -3,7 +3,7 @@
 #Script para realizar backup da pasta de Design.
 #Por fazer muitos trabalhos de Design, eu tenho uma pasta
 #que sempre está enchendo com os projetos, para solucionar este problema decidi
-#fazer um script para arquivar e comprimir todos esses arquivos sempre que chamar.
+#fazer um script para arquivar e comprimir todos esses arquivos sempre que chama                                                                                                 r.
 #
 #Feito por: DevChimpa dia 08 de Janeiro de 2023
 #contato: chimpadeveloper@gmail.com
@@ -19,7 +19,17 @@ CAMINHO="/home/chimpa/Documents/design"
 
 #COMANDOS:
 
+if [ ! -d /home/backups ]
+then    mkdir -p /home/backups
+        CAMINHO_DESTINO="/home/backups"
+else
+        CAMINHO_DESTINO="/home/backups"
+
+fi
+
 tar -czpvf $NOME_COMPLETO $CAMINHO
+sleep 5
+mv $NOME_COMPLETO $CAMINHO_DESTINO
 
 #echo $NOME_COMPLETO
 #echo $NOME_ARQUIVO
